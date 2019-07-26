67dbb266476b8866cdd7d422f98bc877c8a16e9e
kierepkae <kierepkaedyta@gmail.com>
2019-07-26 10:28:55 +0100

    add test for #release_bike method throwing an error - all test pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,13 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
-
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'raises an error when there is no avaible bikes' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
+
+  it { is_expected.to respond_to :release_bike }
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
@@ -23,4 +24,5 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
 end
