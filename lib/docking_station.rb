f9c92d84d930ce56324c01803170128153555e36
kierepkae <kierepkaedyta@gmail.com>
2019-07-26 10:55:01 +0100

    update method dock to return bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,19 +2,20 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_reader :bike
+  attr_reader :bikes
 
   def initialize
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bike
-    @bike = bike
+    fail 'Docking station is full' if @bikes
+    @bikes = bike
+    bike
   end
 
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' unless @bikes
+    @bikes
   end
 
 end
