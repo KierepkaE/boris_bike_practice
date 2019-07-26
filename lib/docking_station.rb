792461f145e60f83371222beced68f374b985416
kierepkae <kierepkaedyta@gmail.com>
2019-07-26 11:41:13 +0100

    create attr_accesor for capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,12 +2,14 @@ require_relative 'bike'
 
 class DockingStation
   DEFAULT_CAPACITY = 20
+
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
     @bikes = []
   end
 
-  attr_reader :bikes, :bike, :capacity
+  attr_reader :bikes, :bike
+  attr_writer :capacity
 
   def dock(bike)
     fail 'Docking station is full' if full?
