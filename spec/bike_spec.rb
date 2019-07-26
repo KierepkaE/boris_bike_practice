62357cccd84bdd325249e183178b3119013f8767
kierepkae <kierepkaedyta@gmail.com>
2019-07-26 11:57:13 +0100

    update tests - tests pass

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,6 +3,7 @@ describe Bike do
   it {is_expected.to respond_to :working?}
 
   it 'can be reported as a broken' do
-    expect(subjec.report_broken).to be_broken
+    subject.report_broken
+    expect(subject).to be_broken
   end
 end
